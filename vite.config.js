ed95af75c03f64434e4c92ca9cfc7af331451f3c
hmilyld <hmilyld@gmail.com>
2024-04-17 17:17:41 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -51,7 +51,7 @@ export default defineConfig(({ mode }) => {
           ],
         },
         manifest: {
-          name: "今日热榜",
+          name: "小胡热榜",
           short_name: "DailyHot",
           description: "汇聚全网热点，热门尽览无余",
           display: "standalone",
